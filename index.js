2eb822b9ecf0b3ca56b07469d35629a9bd835510
Joshua <yoonj200@gmail.com>
2022-02-05 21:50:58 -0600

    commit tree 28b26bb9ebd780a0f1f484949307c3adf04cb426

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+//1) returns all drivers that match passed in name
+//2) returns matching drivers if case does not match but letters do
+//3) returns an empty array if there is no match
+function findMatching(drivers, string) {
+    let answer = drivers.filter(name => name === string ||name === string.toLowerCase());
+    return answer;
+}
+
+//4) returns a driver if beginning provided letters match
+//5) does not return drivers if only middle or ending letters match
+//6) does not return drivers if only middle or ending letters match
+function fuzzyMatch(drivers, string) {
+    let answer = drivers.filter(name => name.startsWith(string));
+    return answer;
+}
+
+//7) accesses data structure if name matches
+function matchName(drivers, name) {
+  let answer = drivers.filter(driver => driver.name === name);
+  return answer;
+}
+/*
+function matchName(drivers, name){
+  return drivers.filter(function(driver){
+    return driver.name === name
+  })
+}
+*/
